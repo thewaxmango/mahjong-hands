b621b46eb458547f78ff1b5d6de3e24e4b77683b
thewaxmango <mxyy@mxyy.localdomain>
2024-05-27 23:41:23 -0400

    can now change hand

--- static/script.js
+++ static/script.js
@@ -18,137 +18,6 @@ function removeClass(el, className) {
     }
 }
 
-const TILE_BACK_HTML = `<image x="2" y="2" width="60" height="80" href="static/svgs/Back.png"/>
-<rect class="back-border" width="60" height="80" x="2" y="2" rx="6"/>`
-const TILE_TEMPLATE_HTML = `<image x="2" y="2" width="60" height="80" href="static/svgs/Front.png"/>
-<image x="9.5" y="12" width="45" height="60" href="static/svgs/Regular/!REPLACE.svg"/>
-<rect class="tile-border" width="60" height="80" x="2" y="2" rx="6"/>`
-const TILE_TO_FILE = createTileToFile();
-function createTileToFile() {
-    out = {};
-    for (let i = 1; i < 10; i++) {
-        out[`${i}m`] = `Man${i}`;
-        out[`${i}p`] = `Pin${i}`;
-        out[`${i}s`] = `Sou${i}`;
-    }
-
-    out[`ew`] = "Ton";
-    out[`sw`] = "Nan";
-    out[`ww`] = "Shaa";
-    out[`nw`] = "Pei";
-
-    out[`wd`] = "Haku";
-    out[`gd`] = "Hatsu";
-    out[`rd`] = "Chun";
-
-    for (let j = 0; j < 4; j++) {
-        for (let i = 1; i < 10; i++) {
-            if (j == 0 && i == 5) {
-                out[`${i}m${j}`] = `Man${i}-Dora`;
-                out[`${i}p${j}`] = `Pin${i}-Dora`;
-                out[`${i}s${j}`] = `Sou${i}-Dora`;
-            } else {
-                out[`${i}m${j}`] = `Man${i}`;
-                out[`${i}p${j}`] = `Pin${i}`;
-                out[`${i}s${j}`] = `Sou${i}`;
-            }
-        }
-
-        out[`ew${j}`] = "Ton";
-        out[`sw${j}`] = "Nan";
-        out[`ww${j}`] = "Shaa";
-        out[`nw${j}`] = "Pei";
-
-        out[`wd${j}`] = "Haku";
-        out[`gd${j}`] = "Hatsu";
-        out[`rd${j}`] = "Chun";
-    }
-
-    return out;
-}
-
-let current_hand = {};
-let current_yaku = {};
-let current_han = 5;
-let current_fu = -1;
-let current_score = "mangan";
-
-const ALL_YAKU = {};
-const VISIBLE_YAKU = ["riichi", "ippatsu", "double", "rinshan", "chankan", "haitei", "houtei", "chiihou", "tenhou"];
-
-const BACK = 0;
-const REGULAR = 1;
-const HORIZ = 2;
-const HORIZ2 = 3;
-function setTileParams(id, type) {
-    let el = document.getElementById(id);
-    switch(type) {
-        case BACK:
-            el.setAttribute("viewbox", "0 0 64 84");
-            el.setAttribute("width", "64");
-            el.setAttribute("height", "84");
-            break;
-        case REGULAR:
-            el.setAttribute("viewbox", "0 0 64 84");
-            el.setAttribute("width", "64");
-            el.setAttribute("height", "84");
-            break;
-        case HORIZ:
-            console.log("a");
-            el.setAttribute("viewbox", "0 0 84 64");
-            el.setAttribute("width", "84");
-            el.setAttribute("height", "64");
-            break;
-        case HORIZ2:
-            el.setAttribute("viewbox", "0 0 84 126");
-            el.setAttribute("width", "84");
-            el.setAttribute("height", "126");
-            break;
-    }
-}
-
-function setVisibleYaku() {
-}
-function hideYaku() {
-}
-function revealYaku() {
-}
-
-function setHand() {
-}
-function isNumberHan(n) {
-    return (n == current_han);
-}
-function isScore(score_type) {
-    return (score_type == current_score);
-}
-
-function setDora(num_visible, dora) {
-    for (let i = 1; i < 6; i++) {
-        document.getElementById(`dora${i}`).innerHTML = TILE_BACK_HTML;
-        setTileParams(`dora${i}`, BACK);
-    }
-
-    for (let i = 0; i < Math.min(5, num_visible); i++) {
-        document.getElementById(`dora${i+1}`).innerHTML = TILE_TEMPLATE_HTML.replace("!REPLACE", TILE_TO_FILE[dora[i]]);
-        setTileParams(`dora${i+1}`, REGULAR);
-    }
-}
-function setUradora(riichi, num_visible, uradora) {
-    for (let i = 1; i < 6; i++) {
-        document.getElementById(`ura${i}`).innerHTML = TILE_BACK_HTML;
-        setTileParams(`ura${i}`, BACK);
-    }
-    if (!riichi) {
-        return
-    }
-
-    for (let i = 0; i < Math.min(5, num_visible); i++) {
-        document.getElementById(`ura${i+1}`).innerHTML = TILE_TEMPLATE_HTML.replace("!REPLACE", TILE_TO_FILE[uradora[i]]);
-        setTileParams(`ura${i+1}`, REGULAR);
-    }
-}
-
 const han_types = [ 
     {"type": "1han", "n": 1}, 
     {"type": "2han", "n": 2}, 
@@ -289,6 +158,210 @@ function resetScoreButtons() {
                 addClass(button_element, "pts-blue");
             }
         }
+    }
+}
+
+const TILE_BACK_HTML = `<image x="2" y="2" width="60" height="80" href="static/svgs/Back.png"/>
+<rect class="back-border" width="60" height="80" x="2" y="2" rx="6"/>`
+const TILE_REGULAR_HTML = `<image x="2" y="2" width="60" height="80" href="static/svgs/Front.png"/>
+<image x="9.5" y="12" width="45" height="60" href="static/svgs/Regular/!REPLACE.svg"/>
+<rect class="tile-border" width="60" height="80" x="2" y="2" rx="6"/>`
+const TILE_HORIZ_HTML = `<image transform="rotate(90 40 40)" x="2" y="-2" width="60" height="80" href="static/svgs/Front.png"></image>
+<image transform="rotate(90 40 40)" x="9.5" y="8" width="45" height="60" href="static/svgs/Regular/!REPLACE.svg"></image>
+<rect class="tile-border" width="80" height="60" x="2" y="2" rx="6" style=""/>`
+const TILE_HORIZ2_HTML = `<image transform="rotate(90 10 70)" x="5" y="-2" width="60" height="80" href="static/svgs/Front.png"></image>
+<image transform="rotate(90 10 70)" x="11.5" y="8" width="45" height="60" href="static/svgs/Regular/!REPLACE.svg"></image>
+<rect class="tile-border" width="80" height="60" x="2" y="64" rx="6"/>
+<image transform="rotate(90 40 40)" x="2" y="-2" width="60" height="80" href="static/svgs/Front.png"></image>
+<image transform="rotate(90 40 40)" x="9.5" y="8" width="45" height="60" href="static/svgs/Regular/!REPLACE.svg"></image>
+<rect class="tile-border" width="80" height="60" x="2" y="2" rx="6"/>`
+const TILE_HTML = [TILE_BACK_HTML, TILE_REGULAR_HTML, TILE_HORIZ_HTML, TILE_HORIZ2_HTML, TILE_BACK_HTML];
+const TILE_TO_FILE = createTileToFile();
+function createTileToFile() {
+    out = {};
+    for (let i = 1; i < 10; i++) {
+        out[`${i}m`] = `Man${i}`;
+        out[`${i}p`] = `Pin${i}`;
+        out[`${i}s`] = `Sou${i}`;
+    }
+
+    out[`ew`] = "Ton";
+    out[`sw`] = "Nan";
+    out[`ww`] = "Shaa";
+    out[`nw`] = "Pei";
+
+    out[`wd`] = "Haku";
+    out[`gd`] = "Hatsu";
+    out[`rd`] = "Chun";
+
+    for (let j = 0; j < 4; j++) {
+        for (let i = 1; i < 10; i++) {
+            if (j == 0 && i == 5) {
+                out[`${i}m${j}`] = `Man${i}-Dora`;
+                out[`${i}p${j}`] = `Pin${i}-Dora`;
+                out[`${i}s${j}`] = `Sou${i}-Dora`;
+            } else {
+                out[`${i}m${j}`] = `Man${i}`;
+                out[`${i}p${j}`] = `Pin${i}`;
+                out[`${i}s${j}`] = `Sou${i}`;
+            }
+        }
+
+        out[`ew${j}`] = "Ton";
+        out[`sw${j}`] = "Nan";
+        out[`ww${j}`] = "Shaa";
+        out[`nw${j}`] = "Pei";
+
+        out[`wd${j}`] = "Haku";
+        out[`gd${j}`] = "Hatsu";
+        out[`rd${j}`] = "Chun";
+    }
+
+    return out;
+}
+
+let current_hand = {};
+let current_yaku = {};
+let current_han = 5;
+let current_fu = -1;
+let current_score = "mangan";
+
+const ALL_YAKU = {};
+const VISIBLE_YAKU = ["riichi", "ippatsu", "double", "rinshan", "chankan", "haitei", "houtei", "chiihou", "tenhou"];
+
+const BACK = 0;
+const REGULAR = 1;
+const HORIZ = 2;
+const HORIZ2 = 3;
+const HIDE = 4;
+function setTileParams(id, type) {
+    let el = document.getElementById(id);
+    switch(type) {
+        case BACK:
+            el.setAttribute("viewBox", "0 0 64 84");
+            el.setAttribute("width", "64");
+            el.setAttribute("height", "84");
+            break;
+        case REGULAR:
+            el.setAttribute("viewBox", "0 0 64 84");
+            el.setAttribute("width", "64");
+            el.setAttribute("height", "84");
+            break;
+        case HORIZ:
+            el.setAttribute("viewBox", "0 0 84 64");
+            el.setAttribute("width", "84");
+            el.setAttribute("height", "64");
+            break;
+        case HORIZ2:
+            el.setAttribute("viewBox", "0 0 84 126");
+            el.setAttribute("width", "84");
+            el.setAttribute("height", "126");
+            break;
+        case HIDE:
+            el.setAttribute("viewBox", "0 0 0 0");
+            el.setAttribute("width", "0");
+            el.setAttribute("height", "0");
+            break;
+    }
+}
+
+const AGARI_HEADER_ID = ["win-by-tsumo", "win-by-ron"]; 
+const AGARI_TITLE_ID = ["title-tsumo", "title-ron"]; 
+const TSUMO = 0;
+const RON = 1;
+
+const WIND_TEXT = ["「東」East", "「南」South", "「西」West", "「北」North"];
+const EAST = 0;
+const SOUTH = 1;
+const WEST = 2;
+const NORTH = 3;
+
+function setVisibleYaku() {
+}
+function hideYaku() {
+}
+function revealYaku() {
+}
+
+function setWinds(round_wind, seat_wind) {
+    document.getElementById("round-wind").innerText = WIND_TEXT[round_wind];
+    document.getElementById("seat-wind").innerText = WIND_TEXT[seat_wind];
+}
+function setWinBy(win_type, win_tile) {
+    // set header top left
+    document.getElementById(AGARI_HEADER_ID[win_type]).hidden = false;
+    document.getElementById(AGARI_HEADER_ID[win_type^1]).hidden = true;
+
+    // set hand subtitle
+    document.getElementById(AGARI_TITLE_ID[win_type]).hidden = false;
+    document.getElementById(AGARI_TITLE_ID[win_type^1]).hidden = true;
+
+    // set tile
+    document.getElementById("agari-tile").innerHTML = TILE_REGULAR_HTML.replace("!REPLACE", TILE_TO_FILE[win_tile]);
+    setTileParams("agari-tile", REGULAR);
+}
+function setHand(closed_tiles, open_tiles) {
+    // reset 
+    document.getElementById("hand").style.zoom = "1";
+    for (let i = 0; i < 13; i++) {
+        document.getElementById(`closed${i}`).innerHTML = TILE_BACK_HTML;
+        setTileParams(`closed${i}`, HIDE);
+    }
+    for (let i = 0; i < 16; i++) {
+        document.getElementById(`open${i}`).innerHTML = TILE_BACK_HTML;
+        setTileParams(`open${i}`, HIDE);
+    }
+
+    // set
+    for (let i = 0; i < Math.min(13, closed_tiles.length); i++) {
+        document.getElementById(`closed${i}`).innerHTML = TILE_REGULAR_HTML.replace("!REPLACE", TILE_TO_FILE[closed_tiles[i]]);
+        setTileParams(`closed${i}`, REGULAR);
+    }
+    for (let i = 0; i < Math.min(16, (13 - Math.min(13, closed_tiles.length))/3*4, open_tiles.length); i++) {
+        document.getElementById(`open${i}`).innerHTML = TILE_HTML[open_tiles[i].type].replace("!REPLACE", TILE_TO_FILE[open_tiles[i].tile]);
+        setTileParams(`open${i}`, open_tiles[i].type);
+    }
+
+    let scale = 1008.0/(document.getElementById("hand-row-main").getBoundingClientRect().width - 39);
+    if (scale < 1) {
+        document.getElementById("hand").style.zoom = `${scale}`;
+    }
+}
+function isNumberHan(n) {
+    return (n == current_han);
+}
+function isScore(score_type) {
+    return (score_type == current_score);
+}
+
+function setDora(num_visible, dora) {
+    // reset
+    for (let i = 0; i < 5; i++) {
+        document.getElementById(`dora${i}`).innerHTML = TILE_BACK_HTML;
+        setTileParams(`dora${i}`, BACK);
+    }
+
+    // set
+    for (let i = 0; i < Math.min(5, num_visible); i++) {
+        document.getElementById(`dora${i}`).innerHTML = TILE_REGULAR_HTML.replace("!REPLACE", TILE_TO_FILE[dora[i]]);
+        setTileParams(`dora${i}`, REGULAR);
+    }
+}
+function setUradora(riichi, num_visible, uradora) {
+    // reset
+    for (let i = 0; i < 5; i++) {
+        document.getElementById(`ura${i}`).innerHTML = TILE_BACK_HTML;
+        setTileParams(`ura${i}`, BACK);
+    }
+    // no ura if no riichi
+    if (!riichi) {
+        return
+    }
+
+    // set
+    for (let i = 0; i < Math.min(5, num_visible); i++) {
+        document.getElementById(`ura${i}`).innerHTML = TILE_REGULAR_HTML.replace("!REPLACE", TILE_TO_FILE[uradora[i]]);
+        setTileParams(`ura${i}`, REGULAR);
     }
 }
 
