d9b9d668000a82cde4feeb2dd65e76ab8fcf9c78
thewaxmango <77765712+thewaxmango@users.noreply.github.com>
2025-12-01 17:43:45 -0500

    Modify app to use environment variable for port
    Updated app to run on environment-defined port.

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def randompuzzle():
             "yaku": yaku}
     return jsonify(data)
 
-app.debug = True
+# app.debug = True
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
